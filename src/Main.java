770f6b87a86c8e5fad37db340156dd04512aead0
Marla Ingridh <marla.ingridh@gmail.com>
2024-02-07 11:24:34 -0400

    Primeiro Commit. Tabuada de Multiplação

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TabuadaMultiplicacao valor = new TabuadaMultiplicacao();
+        int numero = 5;
+        valor.mostrarTabuada(numero);
+    }
+}
